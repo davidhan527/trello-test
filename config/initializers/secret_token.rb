6b29f01bbce18b2ab80b972986e6acdbe618c3d6
davidhan527 <davidhan527@gmail.com>
2014-05-19 18:37:31 -0400

    < / >

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloTestApp::Application.config.secret_key_base = '80af5314b92c161a4d81cb02036cc02cb6285f8a2e9f1c003ecb4030e78034b1d972e586b06e053cb13d7ce7471eb40833a99a5e3a9ee3616eb139364b20db3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrelloTestApp::Application.config.session_store :cookie_store, key: '_trello_test_app_session'
